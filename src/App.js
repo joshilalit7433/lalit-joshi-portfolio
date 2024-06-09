8a5d039687343fcf7d3cc643cee1e6e1694d04ad
Lalit Joshi <136977205+joshilalit7433@users.noreply.github.com>
2024-06-09 20:01:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,26 @@ import Main from "./Components/Main";
 import Navbar from "./Components/Navbar";
 import Projects from "./Components/Projects";
 import Skills from "./Components/Skills";
+import { ThemeContext } from "./context/Theme";
+import React from "react";
 
 export default function App() {
+
+  const [{ style }] = React.useContext(ThemeContext);
+
+
+
+
   return (
     <>
+    <div style={style}>
     <Navbar/>
     <Main/>
     <Education/>
     <Skills/>
     <Projects/>
     <Contact/>
+    </div>
     </>
   )
 }
