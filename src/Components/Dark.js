2caa728a2160fcc6584eecf6ece5bab5ec415af5
Lalit Joshi <136977205+joshilalit7433@users.noreply.github.com>
2024-06-09 20:00:28 +0530

    Add files via upload

---
+++ src/Components/Dark.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Dark() {
+  return (
+    <div>
+        <img src='./Images/dark.png' className="lg:h-[50px] lg:w-[50px] h-[40px] w-[40px]"></img>
+    </div>
+  )
+}
---
+++ src/Components/Light.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Light() {
+  return (
+    <div>
+
+       <img src='./Images/light.png' className="lg:h-[50px] lg:w-[50px] rounded-full bg-white h-[40px] w-[40px]"></img>
+
+    </div>
+  )
+}
